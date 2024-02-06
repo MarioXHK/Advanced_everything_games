9576ad501c2ff6afc373b57064095b061ea12d52
MarioXHK <marioxhk@gmail.com>
2024-02-06 15:36:57 -0700

    A cookie that can be clicked
    "Your first batch goes in the trash. The neighborhood raccoon barely touches it. "
    Who would put seeds in cookies?

--- Cookie Haven/clickers.py
+++ Cookie Haven/clickers.py
@@ -52,7 +52,7 @@ class circleButton:
         self.hover = False
         self.moff = Vector2(0,0)
     def tick(self,mdown: bool,mousePos: Vector2) -> bool:
-        if self.rad < self.pos.distance_to(mousePos):
+        if self.rad > self.pos.distance_to(mousePos):
             self.hover = True
             if mdown:
                 if not self.clicked:
@@ -74,16 +74,37 @@ class circleButton:
 class cookie(circleButton):
     def __init__(self,cookieType: str,position: Vector2, radius: float, seed = random.randint(0,1000000000)):
         self.seed = seed
-        self.kind = cookieType
+        self.kind = cookieType.lower()
         self.rad = radius
         self.pos = position
         self.clicked = False
         self.hover = False
         self.moff = Vector2(0,0)
+    def tellMeType(self,technical):
+        print("I'm a", end = " ")
+        if self.kind == "cc":
+            print("Chocolate Chip")
+        print(" Cookie.")
+        if technical:
+            print("("+self.kind+") technical ID")
     def render(self,screen: pygame.surface):
-        colors = [(200,200,200),(200,200,200),(200,200,200),(200,200,200),(200,200,200)]
-        if self.kind == "Chocolate Chip":
-            colors[0] = (170,119,51)
-            colors[1] = (187,136,85)
+        tempRad = self.rad
         if self.hover:
-            pygame.draw.circle(screen,colors[0],self.pos)
+            tempRad *= 0.95
+
+        random.seed(self.seed)
+        colors = [(200,200,200),(200,200,200),(200,200,200),(200,200,200),(200,200,200)]
+        if self.kind == "cc":
+            colors[0] = (187,136,85)
+            colors[1] = (170,119,51)
+            colors[2] = (85,51,17)
+
+        pygame.draw.circle(screen,colors[0],self.pos,tempRad)
+        pygame.draw.circle(screen,colors[1],self.pos,tempRad,10)
+        
+        if self.kind == "cc":
+            for i in range(random.randint(1,10)):
+                pygame.draw.circle(screen,colors[2],((random.random()-0.5)*(tempRad*1.1),(random.random()-0.5)*(tempRad*1.1))+self.pos,(random.randint(5,10)*tempRad)/50)
+
+
+        random.seed()
--- Cookie Haven/main.py
+++ Cookie Haven/main.py
@@ -1,3 +1,5 @@
+cookies = 0
+
 import pygame
 import random
 import clickers
@@ -9,6 +11,46 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1200, 800))
 pygame.display.set_caption("Cookie Clicker 2: Cookie Haven")
 
-cursor = True
 
-big_cookie = clickers.cookie()
+music = pygame.mixer.music.load("click.mp3")
+pygame.mixer.music.play(-1)
+
+cursoring = True
+
+big_cookie = clickers.cookie("cc",Vector2(400,400),150)
+
+held = False
+tap = False
+mousePos = (0,0)
+tapped = False
+
+print("Starting game")
+while cursoring:
+    #The input you have
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            cursoring = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if not tapped:
+                tap = True
+                tapped = True
+            held = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            held = False
+            tapped = False
+        if event.type == pygame.MOUSEMOTION:
+            mousePos = Vector2(event.pos)
+    clock.tick(60)
+
+    if big_cookie.tick(tap,mousePos):
+        cookies += 1
+        print("Clicked!")
+    
+    
+    screen.fill((238,204,119))
+    
+    big_cookie.render(screen)
+
+    pygame.display.flip()
+    tap = False
+pygame.quit()
